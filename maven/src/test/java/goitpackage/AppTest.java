e7536175924b9c1c8b22f263140adcf17b97d1bd
Ruslana <145785834+ruslanaprus@users.noreply.github.com>
2024-08-05 10:22:36 +0000

    init & start of maven

---
+++ maven/src/test/java/goitpackage/AppTest.java
@@ -0,0 +1,20 @@
+package goitpackage;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
